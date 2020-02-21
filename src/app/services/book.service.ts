f2ee9f8666dc14c29ccc9b93f059e35c7cd58499
ivietwill <ivietwill@gmail.com>
2020-02-21 14:40:37 +0700

    ini commit

---
+++ src/app/services/book.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { mainUrl } from './config';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  constructor() { }
+}
